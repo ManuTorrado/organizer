9e930367da06effa725ef4f4cc001fc4317b0882
ManuTorrado <manueltorrado@gmail.com>
2022-06-11 22:24:45 -0300

    Creacion del script

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mkdir organized;
+arr=( $(ls $1))
+ARRAY=()
+for ((i=0; i < ${#arr[@]}; i++)); do
+string="${arr[$i]##*.}" 
+if [[ ! " ${ARRAY[*]} " =~ " ${string} " ]]; then
+   ARRAY+=($string)
+fi
+
+done
+
+for ((i=0; i < ${#ARRAY[@]}; i++)); do
+mkdir './organized/'${ARRAY[i]}
+mv $1'/'*.${ARRAY[i]} './organized/'${ARRAY[i]}
+done
+echo ${ARRAY[*]}
+
